f91b6cf7984185cfa21e84ffcb9b484902845502
rubashkin_mikhail@tut.by <pristijpribor@mail.ru>
2020-03-04 11:11:47 +0300

    text correction login

--- src/main/java/com/example/painting/config/WebSecurityConfig.java
+++ src/main/java/com/example/painting/config/WebSecurityConfig.java
@@ -34,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure ( HttpSecurity http ) throws Exception{
         http
                 .authorizeRequests ( )
-                .antMatchers ( "/login","/registration", "/catalog/{id}/image", "/mainCatalog").permitAll ( )
+                .antMatchers ( "/login", "/catalog/{id}/image", "/mainCatalog").permitAll ( )
                 .anyRequest ( ).authenticated ( )
                 .and ( )
                 .formLogin ( )
